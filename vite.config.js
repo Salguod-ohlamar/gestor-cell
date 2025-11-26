bc51f6031b7e302cbd12eb29ab5bc758ff81b186
Douglas Machado Ramalho <dm.ramalho@yahoo.com.br>
2025-11-26 13:39:36 -0300

    fix(vite): Adiciona alias explícito para resolver imports no build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // Adiciona um alias explícito para garantir que o build encontre os componentes
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       '/api': {
