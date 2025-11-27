67f0880028e29c640477c205f5410b7cf3f636cb
Douglas Machado Ramalho <dm.ramalho@yahoo.com.br>
2025-11-27 15:12:01 -0300

    fix(build): Corrige caminho de importação do ThemeContext em VendasPage

--- src/components/VendasPage.jsx
+++ src/components/VendasPage.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import ReciboVenda from './ReciboVenda';
 import Modal from './Modal';
 import { useEstoqueContext } from './EstoqueContext';
-import { useTheme } from '../ThemeContext';
+import { useTheme } from './ThemeContext';
 import { validateCPF, validatePhone } from './formatters.js';
 
 const DashboardCard = ({ icon, title, value, colorClass, isToggleable, showValue, onToggle }) => {
